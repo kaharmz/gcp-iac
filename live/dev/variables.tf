1991c2f656878208975d2fdf5d0592c0a8e3691f
kaharmz <abdkaharmuzakir@gmail.com>
2024-10-04 18:58:16 +0700

    Add Comment

--- live/dev/variables.tf
+++ live/dev/variables.tf
@@ -1,3 +1,4 @@
+#Variable vm
 variable "project_id" {}
 variable "instance_name" {}
 variable "machine_type" {}
@@ -6,6 +7,7 @@ variable "zone" {}
 variable "image" {}
 variable "network" {}
 
+#Variable gke
 variable "cluster_name" {}
 variable "node_count" {}
 variable "disk_size" {}
